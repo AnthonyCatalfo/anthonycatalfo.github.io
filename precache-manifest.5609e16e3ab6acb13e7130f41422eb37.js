8dbf027e082349c27ddb8e3b45f39bb885a09db4
AnthonyCatalfo <anthonycatalfo@gmail.com>
2020-12-03 23:59:22 -0500

    Tic Tac Toe

---
+++ precache-manifest.5609e16e3ab6acb13e7130f41422eb37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fe793f7dd85ac872b5c71e6758267b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "217f9d05882335802259",
+    "url": "/static/css/2.eb1025ee.chunk.css"
+  },
+  {
+    "revision": "48facfeeb3b85c0d529d",
+    "url": "/static/css/main.5e98be75.chunk.css"
+  },
+  {
+    "revision": "217f9d05882335802259",
+    "url": "/static/js/2.f2910f0c.chunk.js"
+  },
+  {
+    "revision": "dea0707b72377ae9f23691f847ea486a",
+    "url": "/static/js/2.f2910f0c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "48facfeeb3b85c0d529d",
+    "url": "/static/js/main.eeebc270.chunk.js"
+  },
+  {
+    "revision": "3a92a3665b5282c531ef",
+    "url": "/static/js/runtime-main.3d6a2ba6.js"
+  }
+]);
